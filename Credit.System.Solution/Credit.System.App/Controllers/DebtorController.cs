bbf2a8812901247645c6e93dbe8690cc5b309af5
João Paulo <jpsilva.sousa97@outlook.com>
2025-04-14 21:05:22 -0300

    Criando as classes debtor.

---
+++ Credit.System.Solution/Credit.System.App/Controllers/DebtorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Credit.System.App.Controllers
+{
+    public class DebtorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Credit.System.Solution/Platform.Entity/Interfaces/IDebtorOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Platform.Entity.ServiceSystem;
+
+namespace Platform.Entity.Interfaces
+{
+    public interface IDebtorOperations
+    {
+        public void InsertDebtor(Debtor debtor);
+        
+    }
+}
